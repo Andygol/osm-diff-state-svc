f190cd60f6d641a161d3441edbc0385e065f7800
Andrii Holovin <Andygol@users.noreply.github.com>
2025-06-07 21:36:19 +0300

    Add Vercel configuration files

--- api/server.js
+++ api/server.js
@@ -201,7 +201,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start server only if this file is run directly
-// Only listen if not testing
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
